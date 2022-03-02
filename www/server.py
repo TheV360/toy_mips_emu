09d93b455203c06aa72d39b7f8626574ee1b0d65
V360 <v360code@gmail.com>
2022-03-02 01:20:49 -0500

    new build (???)

--- www/server.py
+++ www/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import http.server
 import socketserver
 
@@ -11,8 +13,11 @@ Handler.extensions_map[".js"] = "application/javascript"
 Handler.extensions_map[".jsm"] = "application/javascript"
 Handler.extensions_map[".wasm"] = "application/wasm"
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	print("Serving at localhost:" + str(PORT))
-	
-	# This will serve the page until you press Ctrl+C in the terminal.
-	httpd.serve_forever()
+try:
+	with socketserver.TCPServer(("", PORT), Handler) as httpd:
+		print("Serving at localhost:" + str(PORT))
+		
+		# This will serve the page until you press Ctrl+C in the terminal.
+		httpd.serve_forever()
+except KeyboardInterrupt:
+	pass
