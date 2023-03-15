c2b9a2545de98fe153c4bfbd1deda161cdacf6e0
V360 <v360code@gmail.com>
2023-03-14 22:08:01 -0400

    what happens if i

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
+git stash push -u --message "temporary push-to-gh-pages stash"
 trunk build --release
+
+sed -i -e '/\/www\/dist/d' ./.gitignore
+
+# https://stackoverflow.com/a/40178818/
+
+git add .gitignore
+git add www/dist/
+
+git push -d origin gh-pages || true # i am lazy
+git commit -m "temporary push-to-gh-pages commit"
 git subtree push --prefix www/dist origin gh-pages
+# git push origin "$(git subtree split --prefix www/dist master)":gh-pages --force
+git reset HEAD~
+
+git checkout .gitignore
+
+git stash pop
